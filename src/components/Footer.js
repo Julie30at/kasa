da088174888c56a016beee1c7addb33f06d0ab81
Julie AT <julie30at@gmail.com>
2024-09-01 16:00:15 +0200

    modification footer pour maquette et responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import logo from "../assets/FooterLogo.png"
+import logo from "../assets/logo_footer.png"
 import "../styles/Footer.scss"
 
 export function Footer() {
     return (
-        <footer className="footer">
-            <img src={logo} alt="logo" />
-        </footer>
-    )
+      <footer className="footer">
+        <img className="footer_logo" src={logo} alt="logo" />
+        <p className="footer_txt">© 2020 Kasa. All rights reserved</p>
+      </footer>
+    );
 }
