db62a58d13ca9630a0fc52d6862e6be1c10312c7
Julie AT <julie30at@gmail.com>
2024-09-01 15:26:42 +0200

    annimation collapse

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import arrowTop from '../assets/arrow_back_top.png';
-import arrowBottom from '../assets/arrow_back_bottom.png';
 import '../styles/Collapse.scss';
 
 export function Collapse({ title, content }) {
   const [toggle, setToggle] = useState(false);
+  const contentRef = useRef(null);
+  const [height, setHeight] = useState('0px');
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    if (toggle) {
+      setHeight(`${contentRef.current.scrollHeight}px`);
+    } else {
+      setHeight('0px');
+    }
+  }, [toggle]);
 
   return (
-    <div className="collapse">
+    <div className={`collapse ${toggle ? 'expanded' : 'collapsed'}`}>
       <h3 className={`collapse_title ${toggle ? 'expanded' : ''}`}>
         {title}
         <img
           className={`arrow ${toggle ? 'arrow_down' : 'arrow_up'}`}
-          src={toggle ? arrowBottom : arrowTop}
+          src={arrowTop}
           alt="Toggle content"
-          onClick={() => setToggle(!toggle)}
+          onClick={handleToggle}
         />
       </h3>
 
-      {toggle && (
-        <div className="collapse_content">
+      <div
+        className="collapse_content-wrapper"
+        style={{
+          height: toggle ? height : '0px',
+          padding: toggle ? '0px' : '0px',
+        }}
+      >
+        <div className="collapse_content" ref={contentRef}>
           {Array.isArray(content)
             ? content.map((item, index) => <p key={index}>{item}</p>)
             : content}
         </div>
-      )}
+      </div>
     </div>
   );
 }
