50a0f0c0909dcb68ad8fe37cf7ebddf7bbb27423
Julie AT <julie30at@gmail.com>
2024-09-08 15:21:50 +0200

    optimisation de la collapse

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,47 +1,41 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import arrowTop from '../assets/arrow_back_top.png';
 import '../styles/Collapse.scss';
 
 export function Collapse({ title, content }) {
   const [toggle, setToggle] = useState(false);
   const contentRef = useRef(null);
-  const [height, setHeight] = useState('0px');
 
   const handleToggle = () => {
-    setToggle(!toggle);
+    setToggle((prevToggle) => !prevToggle); 
   };
 
-  useEffect(() => {
-    if (toggle) {
-      setHeight(`${contentRef.current.scrollHeight}px`);
-    } else {
-      setHeight('0px');
-    }
-  }, [toggle]);
+  const isExpanded = toggle ? 'expanded' : '';
+  const arrowDirection = toggle ? 'arrow_down' : 'arrow_up';
 
   return (
-    <div className={`collapse ${toggle ? 'expanded' : 'collapsed'}`}>
-      <h3 className={`collapse_title ${toggle ? 'expanded' : ''}`}>
-        {title}
+    <div className={`collapse ${isExpanded}`}>
+      {' '}
+      <h3 className={`collapse_title ${isExpanded}`}>
+        {' '}
+        {title} 
         <img
-          className={`arrow ${toggle ? 'arrow_down' : 'arrow_up'}`}
-          src={arrowTop}
+          className={`arrow ${arrowDirection}`} 
+          src={arrowTop} 
           alt="Toggle content"
-          onClick={handleToggle}
+          onClick={handleToggle} 
         />
       </h3>
-
       <div
         className="collapse_content-wrapper"
         style={{
-          height: toggle ? height : '0px',
-          padding: toggle ? '0px' : '0px',
+          height: toggle ? `${contentRef.current?.scrollHeight}px` : '0px',
         }}
       >
         <div className="collapse_content" ref={contentRef}>
           {Array.isArray(content)
-            ? content.map((item, index) => <p key={index}>{item}</p>)
-            : content}
+            ? content.map((item, index) => <p key={index}>{item}</p>) 
+            : content}{' '}
         </div>
       </div>
     </div>
