391ad8eefd0cd089522844de8ada8c3fc66aad5d
Julie AT <julie30at@gmail.com>
2024-09-16 18:18:55 +0200

    supp de la props aboutPage

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,15 +1,15 @@
 import '../styles/Banner.scss';
 
-export function Banner({ aboutPage, backgroundImage, title }) {
+export function Banner({ backgroundImage, title }) {
   const getBannerStyle = () => ({
-    backgroundImage: `linear-gradient(rgba(0, 0, 0, ${aboutPage ? '0.3' : '0.6'}), rgba(0, 0, 0, ${
-      aboutPage ? '0.3' : '0.6'
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, ${title ? '0.6' : '0.3'}), rgba(0, 0, 0, ${
+      title ? '0.6' : '0.3'
     })), url(${backgroundImage})`,
   });
 
   return (
     <section className="banner" style={getBannerStyle()}>
-      {!aboutPage && <h2>{title}</h2>}
+      {title && <h2>{title}</h2>}
     </section>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -33,7 +33,7 @@ export function About() {
   return (
     <div className="about">
       <Header />
-      <Banner aboutPage backgroundImage={aboutPicture} />
+      <Banner backgroundImage={aboutPicture} />
       <main className="about_main">
         {aboutDatas.map((data) => (
           <div key={data.id} className="about_main_collapse">
