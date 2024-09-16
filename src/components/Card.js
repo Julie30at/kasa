b0705ad9927381ab7ab0232a13bd0ac07724b949
Julie AT <julie30at@gmail.com>
2024-09-16 15:01:00 +0200

    optimisation de Cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,7 @@
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import '../styles/Card.scss';
 
 export default function Card({ title, cover }) {
-  const navigate = useNavigate();
-  
-  const isValidTitle = (title) => {
-    return title && title.trim().length > 0;
-  };
-
-  if (!isValidTitle(title)) {
-    navigate('/error'); 
-    return null;
-  }
-
   return (
     <Link to={`/Rental/${title}`} className="gallery_card">
       <img src={cover} alt={`Kasa - ${title}`} />
